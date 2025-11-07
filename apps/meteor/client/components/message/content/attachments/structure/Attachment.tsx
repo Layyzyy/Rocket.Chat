b1114e1fe95eb72b9e1c0d1045c38302959c9b15
Lay Sandeep Shah <layyzy@Lays-MacBook-Pro.local>
2025-11-07 13:10:27 +0530

    fix: allow longer lines in attachment text messages

--- apps/meteor/client/components/message/content/attachments/structure/Attachment.tsx
+++ apps/meteor/client/components/message/content/attachments/structure/Attachment.tsx
@@ -1,29 +1,22 @@
-import { css } from '@rocket.chat/css-in-js';
 import { Box } from '@rocket.chat/fuselage';
-import { useAttachmentDimensions } from '@rocket.chat/ui-contexts';
 import type { ComponentPropsWithoutRef } from 'react';
 
-const className = css`
-	white-space: normal;
-`;
+type AttachmentTextProps = ComponentPropsWithoutRef<typeof Box>;
 
-type AttachmentProps = ComponentPropsWithoutRef<typeof Box>;
+const AttachmentText = (props: AttachmentTextProps) => (
+	<Box
+		data-qa-type='message-attachment'
+		mbe={4}
+		mi={2}
+		fontScale='p2'
+		color='default'
+		style={{
+			whiteSpace: 'pre-wrap',
+			wordBreak: 'break-word',
+			overflowWrap: 'anywhere',
+		}}
+		{...props}
+	/>
+);
 
-const Attachment = (props: AttachmentProps) => {
-	const { width } = useAttachmentDimensions();
-	return (
-		<Box
-			rcx-message-attachment
-			mb={4}
-			maxWidth={width}
-			width='full'
-			display='flex'
-			overflow='hidden'
-			flexDirection='column'
-			className={className}
-			{...props}
-		/>
-	);
-};
-
-export default Attachment;
+export default AttachmentText;
